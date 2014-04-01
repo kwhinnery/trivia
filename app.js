86a0a34b3736a109b7811c3d1b022990c0cf6b90
Kevin Whinnery <kevin.whinnery@gmail.com>
2014-04-01 14:51:44 -0500

    revert

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-var server = require('http').createServer();
-
-server.on('request', function(request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'application/json'
-    });
-    response.end(JSON.stringify(process.env));
-});
-
-server.listen(process.env.PORT||3000);
-
-/*var app = require('gopher'),
+var app = require('gopher'),
     mongoose = require('mongoose');
 
 // Setup mongoose ODM
-mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
+mongoose.connect(process.env.MONGOHQ_URL);
 
 // Configure app routes
-require('./controllers')(app);*/
+require('./controllers')(app);
