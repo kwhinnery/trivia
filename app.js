efc821dd8a0f82fe1eeb9f35266f7584c2546a48
Kevin Whinnery <kevin.whinnery@gmail.com>
2014-04-01 14:50:22 -0500

    testing env info on azure

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var app = require('gopher'),
+var server = require('http').createServer();
+
+server.on('request', function(request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    response.end(JSON.stringify(process.env));
+});
+
+server.listen(process.env.PORT||3000);
+
+/*var app = require('gopher'),
     mongoose = require('mongoose');
 
 // Setup mongoose ODM
-mongoose.connect(process.env.MONGOHQ_URL);
+mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
 
 // Configure app routes
-require('./controllers')(app);
+require('./controllers')(app);*/
